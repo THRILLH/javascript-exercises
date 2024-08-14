1481f53d7d8885d20b570374e926a4e86076b9ed
Matt Seton <mmseton@gmail.com>
2024-08-14 16:32:50 -0300

    Complete ex 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newString = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+    let newNewString = newString.replace(/\s+/g, '');
+    let newNewNewString = newNewString.toLowerCase();
+    let textArray = newNewNewString.split("");
+    let reverseArray = textArray.reverse();
+    let finalString = reverseArray.join("");
+    console.log(newNewString);
+    console.log(finalString);
+    if (newNewNewString === finalString) return true;
+    else return false;
 };
-
+palindromes("Racecar!");
 // Do not edit below this line
 module.exports = palindromes;
