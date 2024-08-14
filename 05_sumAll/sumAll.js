310a56627cc7b2df2d480faf3d90204996fcdfa6
Matt Seton <mmseton@gmail.com>
2024-08-14 11:17:01 -0300

    Create fuction to sum numbers using an array

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let newArray = [];
+    newArray.push(num1)
+    for (i = num1; i < num2; i++) {
+        if (newArray.at(-1) < num2) {
+            newArray.push((newArray.at(-1)) + 1);
+        }
+    }
+    console.log(newArray);
+    let result = newArray.reduce((a, b) => a + b, 0);
+    return console.log(result);
 };
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
