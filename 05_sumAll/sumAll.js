49b9418191f22e51295441646097a3707956dd26
Matt Seton <mmseton@gmail.com>
2024-08-14 11:55:02 -0300

    Simplify code by removing if function on array

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -18,11 +18,8 @@ const sumAll = function(num1, num2) {
     let newArray = [];
     newArray.push(num1)
     for (i = num1; i < num2; i++) {
-        if (newArray.at(-1) < num2) {
             newArray.push((newArray.at(-1)) + 1);
-        }
     }
-    console.log(newArray);
     let result = newArray.reduce((a, b) => a + b, 0);
     return result;
 
