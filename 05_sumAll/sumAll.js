47a3272107cddfc46f710f1e2034dad318e89560
Matt Seton <mmseton@gmail.com>
2024-08-14 11:49:47 -0300

    Finished exercise 5 using arrays

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
 const sumAll = function(num1, num2) {
+    if ((typeof num1 !== 'number') || (typeof num2 !== 'number')) {
+        return "ERROR";
+    }
+    else if ((num1 < 0 ) || (num2 < 0)) {
+        return "ERROR";
+    }
+    else if ((num1 % 1 !== 0) || num2 % 1 !== 0) {
+        return "ERROR";
+    }
+    
+    // Swap variables if num1 is bigger than num2
+    else if (num1 > num2) {
+        let tmp = num1;
+        num1 = num2;
+        num2 = tmp;
+    }
     let newArray = [];
     newArray.push(num1)
     for (i = num1; i < num2; i++) {
@@ -8,9 +24,8 @@ const sumAll = function(num1, num2) {
     }
     console.log(newArray);
     let result = newArray.reduce((a, b) => a + b, 0);
-    return console.log(result);
-};
-sumAll(1, 4);
+    return result;
 
+};
 // Do not edit below this line
 module.exports = sumAll;
